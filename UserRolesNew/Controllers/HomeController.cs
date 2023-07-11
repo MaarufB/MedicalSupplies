59b743a11b0b4cfe6dea39b378e3e1d76b7124df
arjunsagarkpc <mail.arjunsagar@gmail.com>
2023-07-11 17:34:12 -0400

    DoctorNPI lookup view and controller action added. Made changes to other views too to include the Doctor link

--- UserRolesNew/Controllers/HomeController.cs
+++ UserRolesNew/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace UserRolesNew.Controllers
             return View();
         }
 
+        public IActionResult NPILookup()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
