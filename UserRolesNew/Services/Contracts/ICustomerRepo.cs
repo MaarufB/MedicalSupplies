1a6797d8c9fa36d2bafe05287171edbdc5c47551
arjunsagarkpc <mail.arjunsagar@gmail.com>
2023-07-21 02:23:27 -0400

    Made changes to the Customer Create View and Bound the form to the Create Post Action

--- UserRolesNew/Services/Contracts/ICustomerRepo.cs
+++ UserRolesNew/Services/Contracts/ICustomerRepo.cs
@@ -1,9 +1,20 @@
-﻿using UserRolesModels;
+﻿using System.Net;
+using UserRolesModels;
 
 namespace UserRolesNew.Services.Contracts
 {
     public interface ICustomerRepo
     {
         public List<Customer> GetAllCustomers();
+
+        public void AddCustomer(Customer customer);
+
+        public void AddCustomerAddress(CustomerAddress address);
+
+        public int GetStateIdByName(string state);
+
+        public void AddCustomerNumber(CustomerNumber number);
+
+        public void AddCustomerInsurance(Insurance insurance);
     }
 }
