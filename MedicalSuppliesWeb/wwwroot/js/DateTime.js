b540c1a8c5fac85bb955bcb187bc480026dc8137
arjunsagarkpc <mail.arjunsagar@gmail.com>
2023-07-23 23:45:12 -0400

    All pages for: AdminDashboard, Customer and Home have been refactored with shared views

---
+++ MedicalSuppliesWeb/wwwroot/js/DateTime.js
@@ -0,0 +1,16 @@
+﻿
+    function updateTime() {
+            var currentDate = new Date();
+    var date = currentDate.toDateString();
+    var time = currentDate.toLocaleTimeString();
+    var dateElement = document.querySelector('.date');
+    var timeElement = document.querySelector('.time');
+    dateElement.textContent = date;
+    timeElement.textContent = time;
+        }
+
+    // Update the time every second
+    setInterval(updateTime, 1000);
+
+    // Initial update
+    updateTime();
