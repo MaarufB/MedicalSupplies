a7a5057b94f49446b9ee20eb1f6da26746e7ba0d
arjunsagarkpc <mail.arjunsagar@gmail.com>
2023-07-29 06:06:38 -0400

    Modified the left column text size

--- MedicalSuppliesWeb/Controllers/FacilitiesController.cs
+++ MedicalSuppliesWeb/Controllers/FacilitiesController.cs
@@ -1,4 +1,5 @@
-﻿using MedicalSuppliesServices.Services.Contracts;
+﻿using MedicalSuppliesModels;
+using MedicalSuppliesServices.Services.Contracts;
 using MedicalSuppliesWeb.ViewModels.Country;
 using MedicalSuppliesWeb.ViewModels.Facility;
 using MedicalSuppliesWeb.ViewModels.State;
@@ -45,6 +46,61 @@ namespace MedicalSuppliesWeb.Controllers
             }).ToList();
 
             return View(facilityViewModel);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var viewModel = new FacilityVm();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(FacilityVm viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                
+                return View(viewModel);
+            }
+
+            
+            var newFacilityEntity = new Facility
+            {
+                FacilityName = viewModel.FacilityName,
+                ContactName = viewModel.ContactName,
+                ContactEmail = viewModel.ContactEmail
+            };
+
+            
+            foreach (var address in viewModel.FacilityAddresses)
+            {
+                newFacilityEntity.FacilityAddresses.Add(new FacilityAddress
+                {
+                    Address = address.Address,
+                    City = address.City,
+                    StateId = address.StateId, 
+                    CountryId = address.Country.Id, 
+                    Zip = address.Zip
+                });
+            }
+
+            
+            foreach (var number in viewModel.FacilityNumbers)
+            {
+                newFacilityEntity.FacilityNumbers.Add(new FacilityNumber
+                {
+                    PhoneNumber = number.PhoneNumber
+                });
+            }
+
+            // Save the new Facility entity to the database
+            //_facilityRepo.AddFacility(newFacilityEntity);
+            //_facilityRepo.SaveChanges();
+
+            // Redirect to the Details page for the newly created Facility or any other appropriate action
+            return RedirectToAction("Details", new { id = newFacilityEntity.FacilityId });
         }
     }
 }
